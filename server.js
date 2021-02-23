00a29f8d242015811f6f25a6cc614672af1a0ff8
ymcla <ymclapp@yahoo.com>
2021-02-23 12:18:54 -0600

    still trying to get api info to appear

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
 'use strict';
 
 require('dotenv').config();
-
+const cors = require('cors');
 
 const express = require('express');
 const app = express();
-const ejs = require('ejs');
+const pg = require('pg');
+// const methodOverride = require('method-override'; //need when we add/delete)
+// const ejs = require('ejs');
 // const expressLayouts = require('express-ejs-layouts');
 // const favicon = require('serve-favicon');
+const superagent = require('superagent');
 
-const pg = require('pg');
 pg.defaults.ssl = process.env.NODE_ENV === 'production' && { rejectUnauthorized: false };
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => console.error(err));
 
-const superagent = require('superagent');
-
-const cors = require('cors');
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+// app.use(methodOverride('_method'));  //goes with methodOverride above
 
 const PORT = process.env.PORT || 3000;
 console.log('Server is running on port: ', PORT)
 
-app.use(express.static('./public'));
-app.use(cors());
-app.use(express.json());
+// const path = require('path');
+// app.use(express.json());
 // app.use(expressLayouts);
 
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/', (request, response) => {
   response.render('pages/index');
@@ -37,11 +41,13 @@ app.get('/new', (request, response) => {
   response.render('pages/searches/new');
 });
 
-app.get('/show', (request, response) => {
-  response.render('pages/searches/show');
-})
+// app.get('/show', (request, response) => {
+//   response.render('pages/searches/show');
+// });
 
 app.get('pages/searches/show', booksHandler);
+app.post('pages/searches/show', booksHandler);
+
 
 
 function booksHandler(request, response) {
@@ -59,8 +65,12 @@ function booksHandler(request, response) {
       let booksData = JSON.parse(booksResponse.text);
       let bookReturn = booksData.items.map(book => {
         return new Book (book);
-      });
-      response.send(bookReturn);  //we will end up putting in the render instead when we move to show
+      })
+      console.log(bookReturn);
+      // let viewModel = {
+      //   books: bookReturn
+      // }
+      // response.render('pages/searches/show', viewModel);  //we will end up putting in the render instead when we move to show
     })
 
     .catch(err => {
@@ -70,7 +80,7 @@ function booksHandler(request, response) {
 }
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
-app.post('pages/searches/show', booksHandler);
+
 
 //Has to be after stuff loads too
 app.use(notFoundHandler);
