3783f638b90e19cc73f51ec2f2bdb1c2007ea830
ymcla <ymclapp@yahoo.com>
2021-02-22 18:53:52 -0600

    made pages. New is showing, index is showing, working on show and api

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+// const expressLayouts = require('express-ejs-layouts');
+// const favicon = require('serve-favicon');
 
 const pg = require('pg');
 pg.defaults.ssl = process.env.NODE_ENV === 'production' && { rejectUnauthorized: false };
@@ -17,10 +20,12 @@ const superagent = require('superagent');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
-console.log("Server is running on port: ", PORT)
+console.log('Server is running on port: ', PORT)
 
 app.use(express.static('./public'));
 app.use(cors());
+app.use(express.json());
+// app.use(expressLayouts);
 
 app.set('view engine', 'ejs');
 
@@ -32,21 +37,32 @@ app.get('/new', (request, response) => {
   response.render('pages/searches/new');
 });
 
+app.get('/show', (request, response) => {
+  response.render('pages/searches/show');
+})
+
+app.get('pages/searches/show', booksHandler);
+
+
 function booksHandler(request, response) {
-  console.log(request.body)
+  console.log(request.body);
   const url = 'https://www.googleapis.com/books/v1/volumes';
 
   superagent.get(url)
     .query({
+      key: process.env.GOOGLE_API_KEY,
       q: `+in${request.body.search}:${request.body.query}`
     })
+    // console.log(request.body.query);
+
     .then((booksResponse) => {
-      let booksData = json.parse(booksResponse.text);
+      let booksData = JSON.parse(booksResponse.text);
       let bookReturn = booksData.items.map(book => {
         return new Book (book);
       });
       response.send(bookReturn);  //we will end up putting in the render instead when we move to show
     })
+
     .catch(err => {
       console.log(err);
       errorHandler(err, request, response);
@@ -54,6 +70,7 @@ function booksHandler(request, response) {
 }
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+app.post('pages/searches/show', booksHandler);
 
 //Has to be after stuff loads too
 app.use(notFoundHandler);
@@ -88,10 +105,10 @@ client.connect() //<<--keep in server.js
     throw `PG error!:  ${err.message}` //<<--these are tics not single quotes
   });
 
-  function Book(booksData) {
-    this.title = booksData.volumeInfo.title;
-    this.authors = booksData.volumeInfo.authors;
-    this.description = booksData.volumeInfo.description;
-    this.image = booksData.volumeInfo.imageLinks.smallThumnail;  //if no image, then we use stock that is in Trello card
-  }
-  }
+function Book(booksData) {
+  this.title = booksData.volumeInfo.title;
+  this.authors = booksData.volumeInfo.authors;
+  this.description = booksData.volumeInfo.description;
+  this.image = booksData.volumeInfo.imageLinks.smallThumnail;  //if no image, then we use stock that is in Trello card
+}
+
